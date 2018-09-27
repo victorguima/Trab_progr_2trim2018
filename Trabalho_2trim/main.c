a2dd63481e1ab39505e7c2102c37aa13fb0737ea
victorguima <43277299+victorguima@users.noreply.github.com>
2018-09-27 15:16:25 -0300

    Mudanças feitas em aula
    Acho que foi a partir daqui que começamos a usar a as structs como globais e ponteiros desse tipo de struct pra poder usar o mesmo valor em outras funções.
    
    Ah, também troquei a cor do console pq eu sou muito gay.

--- Trabalho_2trim/main.c
+++ Trabalho_2trim/main.c
@@ -5,24 +5,50 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include <locale.h>
 #include <ctype.h>
 #include <string.h>
 #include <conio.h>
 #include <time.h>
 
+typedef unsigned short WORD; // 2 Bytes
+typedef unsigned int   DWORD;// 4 Bytes
+
+struct bmpheader {
+	WORD	bfType;         //Assinatura do arquivo (BM)
+	DWORD	bfSize;         //Tamanho do arquivo
+	WORD	bfReserved1;    //Reservado
+	WORD	bfReserved2;    //Reservado
+	DWORD	bfOffBits;      //Numero de bytes do cabeçalho até o começo do arquivo
+    };
+
+struct bmpinfoheader{
+    DWORD biSize;           //Tamanho do cabeçalho, em bytes
+    DWORD biWidth;          //Largura em pixels
+    DWORD biHeight;         //Altura em pixels
+    WORD biPlanes;          //specifies the number of color planes, must be 1
+    WORD biBitCount;        //Números de bits por pixel
+    DWORD biCompression;    //specifies the type of compression
+    DWORD biSizeImage;      //size of image in bytes
+    DWORD biXPelsPerMeter;  //number of pixels per meter in x axis
+    DWORD biYPelsPerMeter;  //number of pixels per meter in y axis
+    DWORD biClrUsed;        //number of colors used by the bitmap
+    DWORD biClrImportant;   //number of colors that are important
+    };
 
 void menu(int *escolha);
-void headerreader(FILE *adr);
+int headerreader(FILE *adr, struct bmpheader *ptrheader,struct bmpinfoheader *ptrinfo);
 
 int main()
 {
-    system("color 0A");//Troca a cor do console para uma totalmente superior
+    system("color 1E");//Troca a cor do console para uma totalmente superior
     setlocale(LC_ALL, "Portuguese");//Aplicando língua como Português
 
     int option = 0,
         flag   = 0;
+
+    struct bmpheader *ptrheader;
+    struct bmpinfoheader *ptrinfo;
 
     FILE *filePtr;
     filePtr = fopen("TesteBmp.bmp","r+b");
@@ -49,7 +75,7 @@ int main()
         {
             case 1:
                 printf("\n");
-                headerreader(filePtr);
+                headerreader(filePtr,ptrheader,ptrinfo);
                 break;
             case 2:
                 break;
@@ -65,6 +91,7 @@ int main()
     while(flag != 2);
     printf("\n");
     fclose(filePtr);
+    return 0;
 }
 
 void menu(int *escolha)
@@ -78,35 +105,12 @@ void menu(int *escolha)
     }
 }
 
-void headerreader(FILE *adr)
+int headerreader(FILE *adr, struct bmpheader *ptrheader,struct bmpinfoheader *ptrinfo)
 {
-  //typedef unsigned short BYTE; // 1 Byte
-    typedef unsigned short WORD; // 2 Bytes
-    typedef unsigned int   DWORD;// 4 Bytes
-
-    struct bmpheader {
-	WORD	bfType;         //Assinatura do arquivo (BM)
-	DWORD	bfSize;         //Tamanho do arquivo
-	WORD	bfReserved1;    //Reservado
-	WORD	bfReserved2;    //Reservado
-	DWORD	bfOffBits;      //Numero de bytes do cabeçalho até o começo do arquivo
-    };
-
-    struct bmpinfoheader{
-    DWORD biSize;           //Tamanho do cabeçalho, em bytes
-    DWORD biWidth;          //Largura em pixels
-    DWORD biHeight;         //Altura em pixels
-    WORD biPlanes;          //specifies the number of color planes, must be 1
-    WORD biBitCount;        //specifies the number of bit per pixel
-    DWORD biCompression;    //spcifies the type of compression
-    DWORD biSizeImage;      //size of image in bytes
-    DWORD biXPelsPerMeter;  //number of pixels per meter in x axis
-    DWORD biYPelsPerMeter;  //number of pixels per meter in y axis
-    DWORD biClrUsed;        //number of colors used by the bitmap
-    DWORD biClrImportant;   //number of colors that are important
-    };
-    struct bmpinfoheader  cabecalho_info;
     struct bmpheader      cabecalho_bmp;
+    struct bmpinfoheader  cabecalho_info;
+
+    /* bmpinfoheader */
     //Lendo assinatura do arquivo
     fseek(adr, 0, SEEK_SET); //Garantindo que começa do começo
     fread(&cabecalho_bmp.bfType, sizeof(WORD), 1, adr);
@@ -121,19 +125,29 @@ void headerreader(FILE *adr)
     fread(&cabecalho_bmp.bfSize, sizeof(DWORD), 1, adr);
     printf("\nO tamanho do arquivo é %x Bytes",cabecalho_bmp.bfSize);
     //Pulando espaços reservados
-    fseek(adr, 2, SEEK_CUR);
+    fseek(adr, 4, SEEK_CUR);
     //Lendo BfOffSetBits
     fread(&cabecalho_bmp.bfOffBits, sizeof(DWORD), 1, adr);
-    printf("\nO deslocamento do cabeçalho até o início do arquivo é %d Bytes",cabecalho_bmp.bfOffBits>>16 );
+    printf("\nO deslocamento do cabeçalho até o início do arquivo é %d Bytes",cabecalho_bmp.bfOffBits );
+
+    /* bmpheader */
     //Lendo tamanho do arquivo
     fread(&cabecalho_info.biSize, sizeof(DWORD), 1, adr);
-    printf("\nO tamanho do cabeçalho é %x Bytes",cabecalho_info.biSize>>16 );
+    printf("\nO tamanho do cabeçalho é %x Bytes",cabecalho_info.biSize);
     //Lendo largura
     fread(&cabecalho_info.biWidth, sizeof(DWORD), 1, adr);
-    printf("\nA largura do arquivo é %d pixels",cabecalho_info.biWidth>>16 );
+    printf("\nA largura do arquivo é %d pixels",cabecalho_info.biWidth);
     //Lendo altura
     fread(&cabecalho_info.biHeight, sizeof(DWORD), 1, adr);
-    printf("\nA altura do arquivo é %d pixels",cabecalho_info.biHeight>>16 );
+    printf("\nA altura do arquivo é %d pixels",cabecalho_info.biHeight);
+    //Pulando BiPlanes
+    fseek(adr, 2, SEEK_CUR);
+    //Lendo biBitCount
+    fread(&cabecalho_info.biBitCount, sizeof(WORD), 1, adr);
+    printf("\nO arquivo possui %d bits por pixel",cabecalho_info.biBitCount);
 
+    ptrheader = &cabecalho_bmp;
+    ptrinfo   = &cabecalho_info;
 
+    return 0;
 }
